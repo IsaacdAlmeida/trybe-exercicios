35186cc1a39a80c791b4d32698b6aa7e393d0f3b
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-02-08 09:53:23 -0300

    exercicios do dia 04

---
+++ fundamentos/bloco-04-introducao-javascript/dia-04-objetos-e-funcoes/exercicioMentoria.js
@@ -0,0 +1,62 @@
+/* Exercício 1 - Crie uma função que calcule a área e o perímetro de um quadrilátero qualquer. 
+Essa função deve receber dois parâmetros: base e altura e retornar um objeto no formato: { area: 0, perimetro: 0 }.
+
+Fórmulas: perimetro = (2base) + (2altura) 
+area = base * altura
+ */
+
+/* function calculoQuadrilatero(base, altura){
+    let resultadoPerimetro = (2 * base) + (2 * altura);
+    let resultadoArea = base * altura;
+
+    let total = {
+        area: resultadoArea,
+        perimetro: resultadoPerimetro,
+    };
+
+    return total;
+};
+
+console.log(calculoQuadrilatero(10, 10));
+ */
+
+/* Exercício 2 - Crie uma função que, dado um array de números inteiros, retorne a quantidade de números pares e ímpares no formato: { pares: 0, ímpares: 0 }
+ */
+
+/* let numeros = [];
+
+for (let index = 1; index <= 20; index += 1){
+    numeros.push(index);
+}
+
+
+function numbersEvenOdds (numeros){
+    let resultado = {
+        pares: 0,
+        impares: 0,
+    }
+    for (let numero of numeros) {
+        if (numero % 2 === 0){
+            resultado.pares += 1;
+        } else {
+            resultado.impares +=1;
+        }
+    }
+    return resultado;
+};
+    
+
+console.log (numbersEvenOdds(numeros))
+ */
+
+/* Exercício 3 - Crie uma função que receba uma string word e outra string ending. 
+Verifique se a string ending é o final da string word. 
+Considere que a string ending sempre será menor que a string word.
+
+Valor de teste: 'trybe' e 'be' valor esperado no retorno da função: true verificaFimPalavra('trybe', 'be');
+Retorno esperado: true
+verificaFimPalavra('joaofernando', 'fernan');
+Retorno esperado: false;
+
+Dica: Use o SPLIT. */
+
