353afdd67d00be325259c8527182ad3d67eec5d0
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-02-08 13:57:05 -0300

    continuacao exercicios

--- fundamentos/bloco-04-introducao-javascript/dia-04-objetos-e-funcoes/exerciciosParteTwo.js
+++ fundamentos/bloco-04-introducao-javascript/dia-04-objetos-e-funcoes/exerciciosParteTwo.js
@@ -1,6 +1,6 @@
 //1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
-/* 
+
 function palindromoCheck(palavra) {
     let dividePalavra = palavra.split("")
     let palavraReversa = dividePalavra.reverse().join("")
@@ -11,7 +11,25 @@ function palindromoCheck(palavra) {
     }
     
 }
-
-console.log(palindromoCheck("natan")); */
+(palindromoCheck("natan"));
 
 // Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function valueOfIndex(integerArray) {
+    let biggestIndex = 0;
+    for (let indice in integerArray){
+        if (integerArray[biggestIndex] < integerArray[indice]){
+            biggestIndex = indice;
+        }
+    }
+    return biggestIndex;
+}
+
+(valueOfIndex([2, 3, 6, 7, 10, 1]));
+
+//Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function mostCharacterName (arrayDeNomes){
+    let maiorNome = 
+
+}
