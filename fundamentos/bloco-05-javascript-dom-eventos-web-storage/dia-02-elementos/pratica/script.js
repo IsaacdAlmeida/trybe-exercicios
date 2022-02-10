e4c7e3034b0113db3fbcf7ba451ee79057d78261
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-02-10 12:30:42 -0300

    exemplos do dia 2/bloco5

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-elementos/pratica/script.js
@@ -0,0 +1,4 @@
+console.log(document.getElementById('start').nextSibling) // retorna o próximo nó do DOM a partir da DIV start
+
+console.log(document.getElementById('start').nextElementSibling) // retorna o próximo ELEMENTO, no caso o <p>
+
