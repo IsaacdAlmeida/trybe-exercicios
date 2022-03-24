d126f313e1bfefd30208d0bf36eca7a540e7878c
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-03-24 13:46:35 -0300

    adiciona pratica dia 02

---
+++ fundamentos/bloco-09-javascript-e-teste-assincrono/dia-02-javascript-assincrono-fetch-api-async-await/pratica/fetch/fetch.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`algo deu errado :( \n${error}`));
+// }
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch (error) {
+    console.log(`algo deu errado :( \n${error}`)
+  }
+}
+
+fetchJoke();
