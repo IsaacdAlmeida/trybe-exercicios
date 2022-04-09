eb04b73f968c37884778b1b16bc790e832bcdf7f
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-04-09 11:43:07 -0300

    pratica do dia 01/bloco11

--- front-end/bloco-11-componentes-estado-eventos-forms-react/dia-01-componentes-estados-eventos/pratica/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-estado-eventos-forms-react/dia-01-componentes-estados-eventos/pratica/fancy-buttons/src/App.js
@@ -2,19 +2,87 @@ import React from "react";
 
 
 class App extends React.Component {
-  handleClick() {
-    console.log('botão clicado');
+  
+  constructor() {
+    super()
+    this.handleclickButtonOne = this.handleclickButtonOne.bind(this)
+    this.handleclickButtonTwo = this.handleclickButtonTwo.bind(this)
+    this.handleclickButtonThree = this.handleclickButtonThree.bind(this)
+    this.state = {
+      clickButtonOne: 0,
+      clickButtonTwo: 0,
+      clickButtonThree: 0
+    }
   }
 
+  handleclickButtonOne() {
+    this.setState(
+      (previousState, _props) => ({
+        clickButtonOne: previousState.clickButtonOne + 1
+      })
+    )
+  }
+
+  handleclickButtonTwo() {
+    this.setState(
+      (previousState, _props) => ({
+        clickButtonTwo: previousState.clickButtonTwo + 1
+      })
+    )
+  }
+
+  handleclickButtonThree() {
+    this.setState(
+      (previousState, _props) => ({
+        clickButtonThree: previousState.clickButtonThree + 1
+      })
+    )
+  }
+
+  collorButton(button) {
+    return button % 2 === 0 ? 'red' : 'green'
+  }
+  
   render() {
+
+    const { clickButtonOne, clickButtonTwo, clickButtonThree } = this.state
+    
     return (
       <div>
-        <button onClick={this.handleClick}>Botão 01</button>
-        <button onClick={this.handleClick}>Botão 02</button>
-        <button onClick={this.handleClick}>Botão 03</button>
+        <button onClick={this.handleclickButtonOne} style={{backgroundColor: this.collorButton(clickButtonOne)}}> esse botão foi clicado: {clickButtonOne} </button>
+        <button onClick={this.handleclickButtonTwo}> esse botão foi clicado: {clickButtonTwo} </button>
+        <button onClick={this.handleclickButtonThree}> esse botão foi clicado: {clickButtonThree} </button>
       </div>
     )
   }
-}
+} 
 
 export default App;
+
+// Posso chamar a função handleClick dentro da classe
+// para fazer referência a função, no evento do botão, é necessário usar o this para referenciar que é a função dentro da classe
+
+
+/* constructor() {
+  super()
+  this.state = {
+    Cliques: 0
+  }
+  this.handleClick = this.handleClick.bind(this);
+}
+
+handleClick() {
+  this.setState((anterior, _props) => ({
+    Cliques: anterior.Cliques + 1
+  }))
+}
+
+render() {
+  return (
+    <div>
+      <button onClick={this.handleClick}>{this.state.Cliques}</button>
+      <button onClick={this.handleClick}>Botão</button>
+      <button onClick={this.handleClick}>Botão 03</button>
+    </div>
+  )
+} */
