0c4391f4da21d900252ab3a713f3c2b0004dfd3e
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-05-16 11:11:00 -0300

    how use redux-thunk

--- front-end/bloco-15-gerenciamento-estado-com-redux/dia-04-usando-redux-no-react-actions-assincronas/redux-thunk/pratica/my-test-app/src/redux/store/index.js
+++ front-end/bloco-15-gerenciamento-estado-com-redux/dia-04-usando-redux-no-react-actions-assincronas/redux-thunk/pratica/my-test-app/src/redux/store/index.js
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux'; // Agora importamos o applyMiddleware
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'; // importar o thunk do redux-thunk
 import rootReducer from '../reducers'
 
-const store = createStore(rootReducer. composeWithDevTools);
+// const store = createStore(rootReducer, applyMiddleware(thunk)); // dentro da create store a gente chama o applyMiddleware passando o thunk como parâmetro
+// Para usar com o compositeWithDevTools fica da seguinte forma: 
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 
 export default store;
