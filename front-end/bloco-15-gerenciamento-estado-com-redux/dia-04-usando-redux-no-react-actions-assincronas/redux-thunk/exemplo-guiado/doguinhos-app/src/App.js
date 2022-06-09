0b7d4b8c4d0b6e783d97dd3d1f6bb24292373b8b
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-06-08 22:37:11 -0300

    modify readme

--- front-end/bloco-15-gerenciamento-estado-com-redux/dia-04-usando-redux-no-react-actions-assincronas/redux-thunk/exemplo-guiado/doguinhos-app/src/App.js
+++ front-end/bloco-15-gerenciamento-estado-com-redux/dia-04-usando-redux-no-react-actions-assincronas/redux-thunk/exemplo-guiado/doguinhos-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchDog } from './store';
 
+
 function App({ isFetching, src, fetchDog }) {
   return (
     isFetching ? <p>Loading</p>
